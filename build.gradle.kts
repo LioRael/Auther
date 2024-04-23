fb8e959a8b903d27b07ebea4840986f35eb564a6
Leosouthey <621862169@qq.com>
2024-04-23 20:37:02 +0800

    Update Java source compatibility to Java 21 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ group = "cn.echoverse"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
