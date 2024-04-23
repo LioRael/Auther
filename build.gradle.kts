93fd2ab1f815cd4909eb6abd8fc97bede059637a
Leosouthey <621862169@qq.com>
2024-04-23 21:41:16 +0800

    Update Kotlin dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
     implementation("org.springframework.data:spring-data-rest-hal-explorer")
     implementation("org.projectlombok:lombok")
