f683f0e0e6c2b5bd4e6b24ca3fe91e6c9b6bd655
Leosouthey <621862169@qq.com>
2024-04-23 23:03:45 +0800

    Update Spring Boot and Kotlin dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.4"
+    id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "cn.echoverse"
@@ -12,7 +12,6 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
@@ -23,25 +22,18 @@ configurations {
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
 }
-
-extra["springAiVersion"] = "0.8.1"
 
 dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.5.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-//    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
-    implementation("org.springframework.data:spring-data-rest-hal-explorer")
-    implementation("org.projectlombok:lombok")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -50,25 +42,9 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
 }
 
-dependencyManagement {
-//    imports {
-//        mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
-//    }
-}
-
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
-}
-
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = "cn.echoverse.auther.AutherApplication"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
